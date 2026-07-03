9f8f77270c82e20f7ca45e5ff7a81e91234852e6
yonatan zewdu <134876268+yonatanzewdu@users.noreply.github.com>
2026-07-03 08:07:32 -0400

    Delete fleet-backend/src/users/dto/ResetPassword.Dto.ts

--- fleet-backend/src/users/dto/ResetPassword.Dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-
-export class ResetPasswordDto {
-  @IsString()
-  @MinLength(6, { message: 'New password must be at least 6 characters.' })
-  newPassword: string;
-}
